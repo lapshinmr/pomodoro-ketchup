6de3d6c98c23505c0361faa0abc245e5d0cb16ee
Mikhail <lapshinmr@gmail.com>
2019-11-14 10:20:14 +0400

    fix rounded progress bar control

--- src/components/TimerBar.vue
+++ src/components/TimerBar.vue
@@ -19,7 +19,8 @@ export default {
   props: ['isSettingsMode'],
   data() {
     return {
-      isTransition: true
+      isTransition: true,
+      circleLength: 2 * Math.PI * 70
     }
   },
   computed: {
@@ -28,27 +29,25 @@ export default {
       'timeInit',
       'isReversedProgressBar'
     ]),
-    progressValue () {
+    ratio () {
       let ratio = this.timeLeft / this.timeInit
       if (this.isReversedProgressBar) {
         ratio = (1 - ratio)
       }
+      return ratio
+    },
+    progressValue () {
       return {
-        'stroke-dasharray': 2 * Math.PI * 70,
-        'stroke-dashoffset': 2 * Math.PI * 70 * ratio,
+        'stroke-dasharray': this.circleLength,
+        'stroke-dashoffset': this.circleLength * this.ratio,
         transition: this.isTansition ? 'all 0.3s' : 'none'
       }
     },
     line () {
-      let ratio = this.timeLeft / this.timeInit
-      if (this.isReversedProgressBar) {
-        ratio = (1 - ratio)
-      }
-      let length = 2 * Math.PI * 70;
       let lineWidth = 0.0025;
       return {
-        'stroke-dasharray': length * lineWidth + ' ' + length * (1 - lineWidth),
-        'stroke-dashoffset': length * ratio
+        'stroke-dasharray': this.circleLength * lineWidth + ' ' + this.circleLength * (1 - lineWidth),
+        'stroke-dashoffset': this.circleLength * this.ratio
       }
     },
   },
@@ -70,41 +69,41 @@ export default {
             vnode.context.isTransition = true;
             return
           };
-          let x = e.clientX - rootWidth / 2
-          let y = (e.clientY - rootHeight / 2) * -1 // rotate coordinates by 180
-          let signX = Math.sign(x)
-          let signY = Math.sign(y)
-          console.log(signX)
+          let [x, y] = [
+            e.clientX - rootWidth / 2,
+            (e.clientY - rootHeight / 2) * -1  // rotate coordinates by 180 degree
+          ]
+          let [signX, signY]  = [ Math.sign(x), Math.sign(y) ]
           let degree = Math.atan(y / x) * 180 / Math.PI
           switch (true) {
-            case (signX <= 0 && signY > 0):
+            case (signX < 0 && signY > 0):
               degree = degree + 90
               break
-            case (signX < 0 && signY < 0):
+            case (signX < 0 && signY <= 0):
               degree = degree + 90
               break
-            case (signX > 0 && signY < 0):
+            case (signX >= 0 && signY < 0):
               degree = degree + 270
               break
-            case (signX > 0 && signY > 0):
+            case (signX >= 0 && signY > 0):
               degree = degree + 270
               break
           }
           let curTime = Math.ceil(vnode.context.timeInit * (1 - degree / 360))
           if (curTime !== vnode.context.timeLeft && curTime % 10 === 0) {
+            if (curTime === 0) {
+              curTime = vnode.context.timeInit
+            }
             vnode.context.setLeftTime(curTime)
           }
           return false;
         }
 
         el.addEventListener('mousedown', (e) => {
-          console.log(e)
           rootWidth = document.querySelector('.root').offsetWidth;
           rootHeight = document.querySelector('.root').offsetHeight;
           vnode.context.isTransition = false;
-          console.log(rootWidth, rootHeight)
-          //vnode.context.isTransition = false;
-          //vnode.context.pauseTimer()
+          vnode.context.pauseTimer()
           document.addEventListener('mousemove', mousemove);
           return false;
         })
@@ -158,10 +157,11 @@ export default {
         stroke: var(--dark)
 
       &.timer-bar__line
-        stroke: var(--super-light)
+        stroke: transparent
+        transition: stroke 0.3s
 
         &:hover
-          stroke: var(--super-dark)
+          stroke: var(--super-light)
           cursor: grab
 
 </style>
